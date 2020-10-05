5ccee51fc7b9bb575989333cbf04c415a2a2bac5
Yahsei <jessesl22@gmail.com>
2020-10-05 16:28:27 -0700

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+#Each Method
+
+def reverse_each_word(string)
+    array = string.split(" ")
+    reverse_array = []
+    array.each do|string|
+      reverse_array << string.reverse
+    end
+    reverse_array.join(" ")
+  end
+  
+  #Collect Method
+  
+  def reverse_each_word(string)
+    array = string.split(" ")
+    reverse_array = []
+    array.collect do|string|
+      reverse_array << string.reverse
+    end
+    reverse_array.join(" ")
+  end
+  
